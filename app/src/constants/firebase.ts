fb65056b730305b18b1ba4dea64797973a640664
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-05 11:15:02 +0100

    feat: add Firebase configuration and navigation constants, including enums for screens and stacks
    Co-authored-by: Rudani Vivek <93070845+vivekrudani@users.noreply.github.com>
    Co-authored-by: Shanaka Anuradha Samarakoon <12231051+shanaka95@users.noreply.github.com>
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

---
+++ app/src/constants/firebase.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
+import { type FirebaseApp, type FirebaseOptions, initializeApp } from 'firebase/app';
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+
+// Firebase Configuration
+const firebaseConfig: FirebaseOptions = {
+  ...Constants.expoConfig?.extra?.firebase
+};
+
+// Initialize Firebase
+const fireApp: FirebaseApp = initializeApp(firebaseConfig);
+
+// Firebase Auth
+const fireAuth = initializeAuth(fireApp, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+// Firestore
+const fireStore = getFirestore(fireApp, 'mumbi-amos');
+
+// Firebase Functions
+const fireFunction = getFunctions(fireApp);
+
+export { fireApp, fireAuth, fireStore, fireFunction };
---
+++ app/src/constants/index.ts
@@ -0,0 +1,6 @@
+// File: index.ts
+// Description: This file re-exports modules from various files.
+// It serves as a central export point for the modules used in the application.
+
+export * from './firebase';
+export * from './screens';
