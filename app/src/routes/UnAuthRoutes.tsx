d15eb9658571ee6f0e2d6af169af115c126481aa
Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>
2024-11-11 23:02:37 +0100

    feat(onboarding-flow): add energy level page Issue#20
    Signed-off-by: Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>

--- app/src/routes/UnAuthRoutes.tsx
+++ app/src/routes/UnAuthRoutes.tsx
@@ -2,13 +2,21 @@
 // Description: This file defines the navigation structure for unauthenticated users using React Navigation.
 // It sets up a stack navigator with routes for various authentication-related screens such as SignIn, SignUp, ForgotPassword, etc.
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Screens } from '@src/constants';
-import { ForgotPassword, Landing, ResetPassword, SignIn, SignUp } from '@src/screens';
-import type React from 'react';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Screens } from "@src/constants";
+import {
+  ForgotPassword,
+  Landing,
+  ResetPassword,
+  SignIn,
+  SignUp,
+  Energypage,
+} from "@src/screens";
+import type React from "react";
 
 // Define the parameter types for the UnAuthRoutes stack navigator
 export type UnAuthRoutesParams = {
+  [Screens.Energypage]: undefined;
   [Screens.Landing]: undefined;
   [Screens.SignIn]: undefined;
   [Screens.SignUp]: undefined;
@@ -30,11 +38,18 @@ export const UnAuthRoutes: React.FC = () => {
       initialRouteName={Screens.Landing}
       screenOptions={{ headerShown: false }}
     >
+      <UnAuthStack.Screen name={Screens.Energypage} component={Energypage} />
       <UnAuthStack.Screen name={Screens.Landing} component={Landing} />
       <UnAuthStack.Screen name={Screens.SignIn} component={SignIn} />
       <UnAuthStack.Screen name={Screens.SignUp} component={SignUp} />
-      <UnAuthStack.Screen name={Screens.ForgotPassword} component={ForgotPassword} />
-      <UnAuthStack.Screen name={Screens.ResetPassword} component={ResetPassword} />
+      <UnAuthStack.Screen
+        name={Screens.ForgotPassword}
+        component={ForgotPassword}
+      />
+      <UnAuthStack.Screen
+        name={Screens.ResetPassword}
+        component={ResetPassword}
+      />
     </UnAuthStack.Navigator>
   );
 };
--- app/src/screens/index.ts
+++ app/src/screens/index.ts
@@ -2,11 +2,12 @@
 // Description: This file re-exports components from various screen files.
 // It serves as a central export point for the screens used in the application.
 
-export * from './ForgotPassword';
-export * from './Home';
-export * from './Landing';
-export * from './Loading';
-export * from './Profile';
-export * from './ResetPassword';
-export * from './SignIn';
-export * from './SignUp';
+export * from "./ForgotPassword";
+export * from "./Home";
+export * from "./Landing";
+export * from "./Loading";
+export * from "./Profile";
+export * from "./ResetPassword";
+export * from "./SignIn";
+export * from "./SignUp";
+export * from "./Energypage";
