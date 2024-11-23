6d02ce2535fc242dedcc1e685acbbefc75cb91a2
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-23 17:59:44 +0100

    feat: add onEnter prop to ContextualQuestionUserName for improved navigation

--- app/src/components/ContextualQuestionUserName.tsx
+++ app/src/components/ContextualQuestionUserName.tsx
@@ -1,13 +1,21 @@
+import { Text } from '@shadcn/components';
+import type { ContextualQuestionProps } from '@src/types';
 import { User } from 'lucide-react-native';
 import { View } from 'react-native';
 import { TextInput } from './FormikTextInput';
-import { Text } from '@shadcn/components';
 
-export const ContextualQuestionUserName: React.FC = () => {
+export const ContextualQuestionUserName: React.FC<ContextualQuestionProps> = (props) => {
+  const { onNext } = props;
   return (
     <View className='flex flex-1 items-stretch justify-center'>
       <Text className='text-2xl text-center font-medium mb-6'>What is your name?</Text>
-      <TextInput lable='Your Name' fieldName='name' className='w-full' leadingIcon={User} />
+      <TextInput
+        lable='Your Name'
+        fieldName='name'
+        className='w-full'
+        leadingIcon={User}
+        onEnter={onNext}
+      />
     </View>
   );
 };
