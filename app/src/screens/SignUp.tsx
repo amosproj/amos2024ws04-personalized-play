b72e32b37231d5e60d7fefad1a555e55d78dfb83
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-23 16:39:32 +0100

    feat: implement SignIn and SignUp screens with form validation and Firebase authentication
    Co-authored-by: Shanaka Anuradha Samarakoon <12231051+shanaka95@users.noreply.github.com>
    Co-authored-by: Rudani Vivek <93070845+vivekrudani@users.noreply.github.com>

--- app/src/screens/SignUp.tsx
+++ app/src/screens/SignUp.tsx
@@ -1,100 +1,78 @@
+import { createUserWithEmailAndPassword } from '@firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { Text } from '@shadcn/components';
-import { Asset } from 'expo-asset';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { Fingerprint, Mail } from '@shadcn/icons';
+import { AppLogo, SubmitButton, TextInput } from '@src/components';
+import { Screens, Stacks, fireAuth } from '@src/constants';
+import type { AppNavigation } from '@src/types';
 import { Formik } from 'formik';
 import type React from 'react';
-import { useCallback, useState } from 'react';
-import { Image, View } from 'react-native';
-import { SubmitButton, TextInput } from 'src/components';
+import { Pressable, View } from 'react-native';
 import * as Yup from 'yup';
 
-import { Screens, Stacks } from '@src/constants';
-import type { AppNavigation } from 'src/types';
+interface SignUpFormData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 export const SignUp: React.FC = () => {
-  // Load the image asset
-  const image = Asset.fromModule(require('../../assets/signup.png')).uri;
-
-  // Define the form state
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  // Define the form data type
-  type SignUpFormData = {
-    email: string;
-    password: string;
-    confirmPassword: string;
-  };
-
-  // Initialize Firebase Auth
-  const fireAuth = getAuth();
   const { navigate } = useNavigation<AppNavigation>();
 
-  // Define the form submission handler
-  const handleSignUp = useCallback(
-    async (data: SignUpFormData) => {
-      console.log('Submitting form:');
-      // Extract the form data
-      const { email, password } = data;
-
-      // Set the submitting state to true
-      setIsSubmitting(true);
-
-      // Attempt to create a new user
-      try {
-        // Create a new user with email and password
-        const { user } = await createUserWithEmailAndPassword(fireAuth, email, password);
-
-        // TODO: Navigate to the login screen
-        navigate(Stacks.UnAuth, { screen: Screens.SignIn });
-      } catch (error) {
-        console.error(error);
-      }
-
-      // Set the submitting state to false
-      setIsSubmitting(false);
-    },
-    [fireAuth, navigate]
-  );
+  const onSignUp = async (values: SignUpFormData) => {
+    const { email, password } = values;
+    try {
+      await createUserWithEmailAndPassword(fireAuth, email, password);
+      navigate(Stacks.UnAuth, { screen: Screens.SignIn });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <View className='flex-1 flex-col justify-center'>
-      <Image
-        source={{ uri: image }}
-        className='w-[95%] h-auto left-16 aspect-square'
-        resizeMode='contain'
-        key={'landing-image'}
-      />
-      <View style={{ padding: 16 }}>
-        <Formik
-          initialValues={{ email: '', password: '', confirmPassword: '' }}
-          validationSchema={Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().min(6).required(),
-            confirmPassword: Yup.string()
-              .required()
-              .oneOf([Yup.ref('password')], 'Passwords must match')
-          })}
-          onSubmit={handleSignUp}
-          validateOnBlur={true}
-          validateOnChange={true}
-        >
-          {({ handleSubmit }) => (
-            <View style={{ marginBottom: 24 }}>
-              <TextInput fieldName='email' lable='Email' keyboardType='email-address' />
-              <TextInput fieldName='password' lable='Password' secureTextEntry={true} />
-              <TextInput
-                fieldName='confirmPassword'
-                lable='Re-enter Password'
-                secureTextEntry={true}
-                submitOnEnter={true}
-              />
-              <SubmitButton>
-                <Text>Sign up</Text>
-              </SubmitButton>
-            </View>
-          )}
-        </Formik>
+    <View className='flex flex-1 justify-center px-6'>
+      <View className='flex flex-col justify-center items-center self-center'>
+        <AppLogo height={100} width={80} />
+        <Text className='text-3xl font-medium mt-4 mb-2'>Sign Up</Text>
+        <Text className='text-lg mb-4'>Signup to get started with our amazing app.</Text>
+      </View>
+      <Formik
+        initialValues={{ email: '', password: '', confirmPassword: '' }}
+        validationSchema={Yup.object({
+          email: Yup.string().email('Invalid email').required('Required'),
+          password: Yup.string().required('Required').min(8, 'Must be 8 characters or more'),
+          confirmPassword: Yup.string()
+            .required('Required')
+            .min(8, 'Must be 8 characters or more')
+            .oneOf([Yup.ref('password')], 'Passwords must match')
+        })}
+        onSubmit={onSignUp}
+      >
+        <View className='flex flex-col justify-center items-stretch my-2'>
+          <TextInput fieldName='email' lable='Email' leadingIcon={Mail} />
+          <TextInput
+            fieldName='password'
+            lable='Password'
+            secureTextEntry={true}
+            leadingIcon={Fingerprint}
+          />
+          <TextInput
+            fieldName='confirmPassword'
+            lable='Confirm Password'
+            secureTextEntry={true}
+            leadingIcon={Fingerprint}
+            submitOnEnter={true}
+          />
+          <SubmitButton className='native:h-14 native:py-2'>
+            <Text className='native:text-xl'>Sign Up</Text>
+          </SubmitButton>
+        </View>
+      </Formik>
+      <View className='flex flex-row justify-center items-center mt-2'>
+        <Text className='text-lg'>Already have an account?</Text>
+        <Pressable onPress={() => navigate(Stacks.UnAuth, { screen: Screens.SignIn })}>
+          <Text className='text-lg text-primary ml-2'>Sign In</Text>
+        </Pressable>
       </View>
     </View>
   );
