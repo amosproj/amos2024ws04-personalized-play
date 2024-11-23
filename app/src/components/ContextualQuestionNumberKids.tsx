20643cc4ab79faee015791f497dfaa709c777f50
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-23 18:02:06 +0100

    feat: update ContextualQuestionNumberKids to accept props and handle onEnter event
    Co-authored-by: Frederick Baier <baier.frederick@gmail.com>

--- app/src/components/ContextualQuestionNumberKids.tsx
+++ app/src/components/ContextualQuestionNumberKids.tsx
@@ -1,9 +1,11 @@
 import { Text } from '@shadcn/components';
+import type { ContextualQuestionProps } from '@src/types';
 import { Baby } from 'lucide-react-native';
 import { View } from 'react-native';
 import { TextInput } from './FormikTextInput';
 
-export const ContextualQuestionNumberKids: React.FC = () => {
+export const ContextualQuestionNumberKids: React.FC<ContextualQuestionProps> = (props) => {
+  const { onNext } = props;
   return (
     <View className='flex flex-1 items-stretch justify-center'>
       <Text className='text-2xl text-center font-medium mb-6'>How many kids do you have?</Text>
@@ -13,6 +15,7 @@ export const ContextualQuestionNumberKids: React.FC = () => {
         className='w-full'
         keyboardType='number-pad'
         leadingIcon={Baby}
+        onEnter={onNext}
       />
     </View>
   );
