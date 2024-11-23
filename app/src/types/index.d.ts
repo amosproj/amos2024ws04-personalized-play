75504200f1c69a82bb27b43a9db5e514f1d67f06
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-23 21:22:26 +0100

    feat: add User and Kid interfaces to define user and child data structures

--- app/src/types/index.d.ts
+++ app/src/types/index.d.ts
@@ -1,8 +1,27 @@
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { AppRoutesParams } from '@src/routes';
+import type { Timestamp } from 'firebase/firestore';
 
 export type AppNavigation = NativeStackNavigationProp<AppRoutesParams>;
 
 export interface ContextualQuestionProps {
   onNext: () => void;
+}
+
+export interface User {
+  displayName: string;
+  email: string;
+  lastSignIn: Timestamp;
+  relationship: string;
+}
+
+export interface Kid {
+  name: string;
+  birthdate: Timestamp;
+  gestationalAge: number;
+  gender: string;
+  healthConsiderations: {
+    isConsidered: boolean;
+    considerations: Array<string>;
+  };
 }
