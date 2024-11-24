69fe759ceb801a37468195e9b4f1aa54a56549c9
Kübra Demirhan <demirhankubra00@gmail.com>
2024-11-24 20:48:45 +0100

    feat(screen): Refactor code using formik for the Issue #55
    Signed-off-by: Kübra Demirhan <demirhankubra00@gmail.com>

--- app/src/components/ContextualQuestionAgeKids.tsx
+++ app/src/components/ContextualQuestionAgeKids.tsx
@@ -1,13 +1,38 @@
-import { Text } from '@shadcn/components';
-import { useField } from 'formik';
-import { View } from 'react-native';
+import { Text } from "@shadcn/components";
+import { useField } from "formik";
+import { View } from "react-native";
+import LottieView from "lottie-react-native";
+import { TextInput } from './FormikTextInput';
+import type { ContextualQuestionProps } from '@src/types';
 
-export const ContextualQuestionAgeKids: React.FC = () => {
-  const [field] = useField('numberOfKids');
+
+export const ContextualQuestionAgeKids: React.FC<ContextualQuestionProps> = (props) => {
+  const [field] = useField("numberOfKids");
+  const { onNext } = props;
   return (
-    <View className='flex flex-1 items-stretch justify-center'>
-      <Text className='text-2xl text-center font-medium mb-6'>Tell us about your kids</Text>
-      <Text>ContextualQuestionAgeKids {field.value}</Text>
+    <View className="flex flex-1 items-stretch justify-center">
+      <View className="flex flex-1 flex-col items-center justify-center mb-4">
+        <LottieView
+          autoPlay={true}
+          loop={true}
+          source={require("../../assets/kids.json")}
+          style={{ width: 320, height: 320 }}
+        />
+      </View>
+      <View className="flex flex-1 flex-col items-stretch">
+        <Text className="text-2xl text-center font-medium mb-6">
+          Tell us about your kids
+        </Text>
+        {/* <Text>ContextualQuestionAgeKids {field.value}</Text> */}
+        <TextInput
+          lable='Age of kids'
+          fieldName='ageOfKids'
+          placeholder="DD-MM-YYYY"
+          className='w-full'
+          keyboardType='number-pad'
+          onEnter={onNext}
+        />
+      </View>
     </View>
   );
 };
