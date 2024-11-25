3a8bcf1e276979bed818054fc407e19183747a59
Preet Vadaliya <preetvadaliya@gmail.com>
2024-11-26 00:20:42 +0100

    feat(ContextualQuestionActivityChoice): replace buttons with ToggleGroup for activity selection
    Co-authored-by: Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- app/src/components/ContextualQuestionActivityChoice.tsx
+++ app/src/components/ContextualQuestionActivityChoice.tsx
@@ -1,7 +1,12 @@
 import { Text } from '@shadcn/components';
+import { ToggleGroup, ToggleGroupIcon, ToggleGroupItem } from '@shadcn/components/ui/toggle-group';
+import { iconWithClassName } from '@shadcn/icons/iconWithClassName';
 import { useFormikContext } from 'formik';
 import LottieView from 'lottie-react-native';
-import { TouchableOpacity, View, type ViewProps } from 'react-native';
+import { CookingPot, Gamepad2, Info } from 'lucide-react-native';
+import { View, type ViewProps } from 'react-native';
+
+iconWithClassName(Info);
 
 export const ContextualQuestionActivityChoice: React.FC<ViewProps> = () => {
   const { setFieldValue, values } = useFormikContext<{ activityType: string }>();
@@ -15,46 +20,29 @@ export const ContextualQuestionActivityChoice: React.FC<ViewProps> = () => {
           style={{ width: 320, height: 320 }}
         />
       </View>
-      <View className='flex flex-1 flex-col items-stretch'>
-        <Text className='text-2xl text-center font-medium mb-6'>
-          What kind of play would you like?
-        </Text>
-        <Text className='text-lg text-center'>
-          Feeling like tackling some chores or just having fun with free play?
-        </Text>
-        <View className='flex flex-1 flex-col items-center'>
-          <View className='flex-row space-x-4'>
-            {/* Chores Button */}
-            <TouchableOpacity
-              onPress={() => setFieldValue('activityType', 'chores')}
-              className={`w-32 h-24 m-4 rounded-lg justify-center items-center ${
-                values.activityType === 'chores' ? 'bg-primary' : 'bg-secondary'
-              }`}
-            >
-              <Text
-                className={`font-bold text-lg ${
-                  values.activityType === 'chores' ? 'text-white' : 'text-black'
-                }`}
-              >
-                Chores
-              </Text>
-            </TouchableOpacity>
-            {/* Freeplay Button */}
-            <TouchableOpacity
-              onPress={() => setFieldValue('activityType', 'freeplay')}
-              className={`w-32 h-24 m-4 rounded-lg justify-center items-center ${
-                values.activityType === 'freeplay' ? 'bg-primary' : 'bg-secondary'
-              }`}
-            >
-              <Text
-                className={` font-bold text-lg ${
-                  values.activityType === 'freeplay' ? 'text-white' : 'text-black'
-                }`}
-              >
-                Freeplay
-              </Text>
-            </TouchableOpacity>
-          </View>
+      <View className='flex flex-1 flex-col items-stretch gap-y-6'>
+        <View className='flex flex-col items-center'>
+          <Text className='text-2xl text-center font-medium mb-4'>Select an activity type</Text>
+          <Text className='text-lg text-center'>
+            Are you looking to do some chores or play today?
+          </Text>
+        </View>
+        <View className='flex-row items-center justify-center gap-x-4'>
+          <ToggleGroup
+            value={values.activityType}
+            onValueChange={(value) => setFieldValue('activityType', value)}
+            type='single'
+            className='gap-x-4'
+          >
+            <ToggleGroupItem value='bold' className='native:h-28 native:w-28 rounded-xl'>
+              <ToggleGroupIcon icon={CookingPot} size={32} />
+              <Text className='text-center text-xl mt-4'>Chores</Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value='italic' className='native:h-28 native:w-28 rounded-xl'>
+              <ToggleGroupIcon icon={Gamepad2} size={32} />
+              <Text className='text-center text-xl mt-4'>Play</Text>
+            </ToggleGroupItem>
+          </ToggleGroup>
         </View>
       </View>
     </View>
