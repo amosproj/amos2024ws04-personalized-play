15c7a28b9e28694eeea83ca0432b143f75b24abc
Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>
2024-11-25 21:05:49 +0100

    feat(ActivityChoice): implemented functionality
    Signed-off-by: Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>

--- app/src/components/ContextualQuestionActivityChoice.tsx
+++ app/src/components/ContextualQuestionActivityChoice.tsx
@@ -1,8 +1,11 @@
 import { Text } from '@shadcn/components';
+import { useFormikContext } from 'formik';
 import LottieView from 'lottie-react-native';
-import { View, type ViewProps } from 'react-native';
+import { TouchableOpacity, View, type ViewProps } from 'react-native';
 
 export const ContextualQuestionActivityChoice: React.FC<ViewProps> = () => {
+  const { setFieldValue, values } = useFormikContext<{ activityType: string }>();
+  console.log(values.activityType);
   return (
     <View className='flex flex-1 items-stretch justify-center'>
       <View className='flex flex-1 flex-col items-center justify-center mb-4'>
@@ -20,7 +23,40 @@ export const ContextualQuestionActivityChoice: React.FC<ViewProps> = () => {
         <Text className='text-lg text-center'>
           Feeling like tackling some chores or just having fun with free play?
         </Text>
-        <View className='h-[48px] items-stretch justify-center'>{/*Insert comp here*/}</View>
+        <View className='flex flex-1 flex-col items-center'>
+          <View className='flex-row space-x-4'>
+            {/* Chores Button */}
+            <TouchableOpacity
+              onPress={() => setFieldValue('activityType', 'chores')}
+              className={`w-32 h-24 m-4 rounded-lg justify-center items-center ${
+                values.activityType === 'chores' ? 'bg-primary' : 'bg-secondary'
+              }`}
+            >
+              <Text
+                className={`font-bold text-lg ${
+                  values.activityType === 'chores' ? 'text-white' : 'text-black'
+                }`}
+              >
+                Chores
+              </Text>
+            </TouchableOpacity>
+            {/* Freeplay Button */}
+            <TouchableOpacity
+              onPress={() => setFieldValue('activityType', 'freeplay')}
+              className={`w-32 h-24 m-4 rounded-lg justify-center items-center ${
+                values.activityType === 'freeplay' ? 'bg-primary' : 'bg-secondary'
+              }`}
+            >
+              <Text
+                className={` font-bold text-lg ${
+                  values.activityType === 'freeplay' ? 'text-white' : 'text-black'
+                }`}
+              >
+                Freeplay
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   );
