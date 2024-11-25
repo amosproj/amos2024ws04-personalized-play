f3e147622af6411d8be86e46558c673920870d6b
Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>
2024-11-25 23:02:11 +0100

    feat(energylevel): implement Formik && edit Slider && add labels
    Signed-off-by: Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>

--- app/src/components/ContextualQuestionEnergyLevel.tsx
+++ app/src/components/ContextualQuestionEnergyLevel.tsx
@@ -1,10 +1,24 @@
 import { Text } from '@shadcn/components';
+import { useField } from 'formik';
 import LottieView from 'lottie-react-native';
 import { View, type ViewProps } from 'react-native';
 import { Slider } from 'react-native-awesome-slider';
 import { useSharedValue } from 'react-native-reanimated';
 
 export const ContextualQuestionEnergyLevel: React.FC<ViewProps> = () => {
+  const [field, , helpers] = useField('energyLevel');
+  const sharedValue = useSharedValue(field.value);
+
+  const handleSlidingComplete = (value: number) => {
+    sharedValue.value = value;
+    helpers.setValue(value);
+  };
+
+  const getLabel = (value: number) => {
+    const labels = ['Empty fuel', 'Half-charged', 'Supercharged'];
+    return labels[value];
+  };
+
   return (
     <View className='flex flex-1 items-stretch justify-center'>
       <View className='flex flex-1 flex-col items-center justify-center mb-4'>
@@ -24,8 +38,12 @@ export const ContextualQuestionEnergyLevel: React.FC<ViewProps> = () => {
           <Slider
             style={{ width: '80%', alignSelf: 'center' }}
             minimumValue={useSharedValue(0)}
-            maximumValue={useSharedValue(100)}
-            progress={useSharedValue(50)}
+            maximumValue={useSharedValue(2)}
+            progress={sharedValue}
+            steps={2}
+            onSlidingComplete={handleSlidingComplete}
+            markWidth={0}
+            bubble={(value) => getLabel(value)}
           />
         </View>
       </View>
