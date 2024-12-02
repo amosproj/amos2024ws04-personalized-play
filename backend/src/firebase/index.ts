15702e3a008238125d57a6f64f31d4b71230c661
Preet Vadaliya <preetvadaliya@gmail.com>
2024-12-02 21:57:49 +0100

    feat: refactor Firebase integration by moving services to a separate file and updating settings
    Co-authored-by: Abderrahmane Bennani <Abderrahmane.Bennani@fau.de>
    Signed-off-by: Preet Vadaliya <preetvadaliya@gmail.com>

--- backend/src/firebase/index.ts
+++ backend/src/firebase/index.ts
@@ -1,15 +1,1 @@
-import vertexAI from '@genkit-ai/vertexai';
-import { initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
-import { genkit } from 'genkit';
-
-const fireApp = initializeApp();
-const fireAuth = getAuth(fireApp);
-const fireStore = getFirestore(fireApp);
-
-const mumbiAI = genkit({
-  plugins: [vertexAI({ projectId: 'mumbi-amos', location: 'us-central1' })]
-});
-
-export { fireApp, fireAuth, fireStore, mumbiAI };
+export * from './services';
