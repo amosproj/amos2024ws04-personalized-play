293c01beacbf9140dede2a36bf362b84a89d032b
Preet Vadaliya <preetvadaliya@gmail.com>
2025-01-12 23:04:41 +0100

    refactor: add ActivityPlayer screen and Skills constants for improved structure

--- app/src/constants/screens.ts
+++ app/src/constants/screens.ts
@@ -7,7 +7,8 @@ export enum Screens {
   Home = 'Home',
   Profile = 'Profile',
   Welcome = 'Welcome',
-  Onboarding = 'Onboarding'
+  Onboarding = 'Onboarding',
+  ActivityPlayer = 'ActivityPlayer'
 }
 
 export enum Stacks {
