9edaae8bf6a31fe33c867ae1bed11255020df5fc
Shanaka Anuradha <shanaka95@gmail.com>
2025-01-14 19:03:43 +0100

    Refactored image capturing process - UI Fixes for camera page

--- app/src/components/ContextualQuestionCamera.tsx
+++ app/src/components/ContextualQuestionCamera.tsx
@@ -5,10 +5,13 @@ import * as ImagePicker from 'expo-image-picker';
 import { useFormikContext } from 'formik';
 import LottieView from 'lottie-react-native';
 import type React from 'react';
+import { useState } from 'react';
 import { View } from 'react-native';
+import { Loading } from './Loading';
 
 export const ContextualQuestionCamera: React.FC<ContextualQuestionProps> = ({ onNext, component, toggleNext }) => {
   const { setFieldValue, values } = useFormikContext<{ camera: string }>();
+  const [loading, setLoading] = useState(false);
 
   const takePhoto = async () => {
     // Request camera permissions
@@ -19,6 +22,9 @@ export const ContextualQuestionCamera: React.FC<ContextualQuestionProps> = ({ on
       return;
     }
     
+    // Set loading state
+    setLoading(true);
+
     // Launch camera
     const result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -26,34 +32,44 @@ export const ContextualQuestionCamera: React.FC<ContextualQuestionProps> = ({ on
     });
 
     if (!result.canceled) {
+      setLoading(false);
       setFieldValue('camera', result.assets[0].uri);
       onNext();
+
     }
   };
 
   return (
-    <View className='flex flex-1 items-stretch justify-center'>
-      <View className='flex flex-1 flex-col items-center justify-center mb-4'>
-        <LottieView
-          autoPlay={true}
-          loop={true}
-          source={require('../../assets/activity.json')}
-          style={{ width: 320, height: 320 }}
-        />
-      </View>
-      <View className='flex flex-1 flex-col items-stretch gap-y-6'>
-        <View className='flex flex-col items-center'>
-          <Text className='text-2xl text-center font-medium mb-4'>
-            Got some supplies nearby? Snap a pic, and we’ll turn them into fun ideas!
-          </Text>
-        </View>
-        <View className='w-full'>
-          <Button className='flex flex-row gap-x-5' onPress={takePhoto}>
-            <Camera size={24} className='text-white' />
-            <Text>Take Picture</Text>
-          </Button>
-        </View>
-      </View>
+    <View className="flex-1 p-4 bg-white">
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <View className='flex flex-1 items-stretch justify-center'>
+            <View className='flex flex-1 flex-col items-center justify-center mb-4'>
+              <LottieView
+                autoPlay={true}
+                loop={true}
+                source={require('../../assets/activity.json')}
+                style={{ width: 320, height: 320 }}
+              />
+            </View>
+            <View className='flex flex-1 flex-col items-stretch gap-y-6'>
+              <View className='flex flex-col items-center'>
+                <Text className='text-2xl text-center font-medium mb-4'>
+                  Got some supplies nearby? Snap a pic, and we’ll turn them into fun ideas!
+                </Text>
+              </View>
+              <View className='w-full'>
+                <Button className='flex flex-row gap-x-5' onPress={takePhoto}>
+                  <Camera size={24} className='text-white' />
+                  <Text>Take Picture</Text>
+                </Button>
+              </View>
+            </View>
+          </View>
+        </>
+      )}
     </View>
   );
 };
