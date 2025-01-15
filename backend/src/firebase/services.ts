5413d1bd5117eb7dbe486c7781e6b633e4be7723
Preet Vadaliya <preetvadaliya@gmail.com>
2025-01-15 03:43:00 +0100

    feat: integrate Google Cloud Text-to-Speech for chore instructions audio generation

--- backend/src/firebase/services.ts
+++ backend/src/firebase/services.ts
@@ -1,14 +1,23 @@
 import vertexAI, { gemini15Pro } from '@genkit-ai/vertexai';
+import { TextToSpeechClient } from '@google-cloud/text-to-speech';
 import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getStorage } from 'firebase-admin/storage';
+import { defineString } from 'firebase-functions/params';
 import { genkit } from 'genkit';
 
 // firebase services
 const fireApp = initializeApp();
 const fireAuth = getAuth(fireApp);
-const fireStore = getFirestore(fireApp);
+const fireStore = getFirestore(fireApp, 'mumbi-amos');
 fireStore.settings({ ignoreUndefinedProperties: true });
+const fireStorage = getStorage(fireApp);
+
+// google cloud services
+const gcloudTTS = new TextToSpeechClient({
+  apiKey: defineString('GOOGLE_TTS_API').value()
+});
 
 // firebase ai services
 const fireGenkit = genkit({
@@ -16,4 +25,4 @@ const fireGenkit = genkit({
   model: gemini15Pro
 });
 
-export { fireApp, fireAuth, fireStore, fireGenkit };
+export { fireApp, fireAuth, fireStore, fireGenkit, fireStorage, gcloudTTS };
